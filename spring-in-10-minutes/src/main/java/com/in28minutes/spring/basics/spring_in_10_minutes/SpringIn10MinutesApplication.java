38aff5f45e148d6bba4a80b9739e8bd61e082183
Brayanova <silviya.brayanova@sap.com>
2020-09-18 14:41:37 +0300

    Initial commit

---
+++ spring-in-10-minutes/src/main/java/com/in28minutes/spring/basics/spring_in_10_minutes/SpringIn10MinutesApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.spring.basics.spring_in_10_minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn10MinutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIn10MinutesApplication.class, args);
+	}
+
+}
---
+++ spring-in-10-minutes/src/test/java/com/in28minutes/spring/basics/spring_in_10_minutes/SpringIn10MinutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.spring.basics.spring_in_10_minutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIn10MinutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
