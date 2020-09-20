33c5294590aea3f7b92e48e4071a0f465f06ee9e
Brayanova <silviya.brayanova@sap.com>
2020-09-20 21:06:54 +0300

    Add springboot initial project

---
+++ springboot-in-10-steps/src/test/java/com/in28minutes/springboot/basics/springboot_in_10_steps/SpringbootIn10StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.basics.springboot_in_10_steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIn10StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
