d1981f6debbd19888949bfc9a17edde36bc15440
Brayanova <silviya.brayanova@sap.com>
2020-09-24 21:59:07 +0300

    Add Books management and enable actuators

---
+++ springboot-in-10-steps/src/main/java/com/in28minutes/springboot/basics/springboot_in_10_steps/HttpTraceActuatorConfiguration.java
@@ -0,0 +1,15 @@
+package com.in28minutes.springboot.basics.springboot_in_10_steps;
+
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@Profile("actuator-endpoints") /* if you want: register bean only if profile is set */
+public class HttpTraceActuatorConfiguration {
+	 @Bean
+	    public HttpTraceRepository httpTraceRepository() {
+	        return new InMemoryHttpTraceRepository();
+	    }
+}
--- springboot-in-10-steps/src/main/java/com/in28minutes/springboot/basics/springboot_in_10_steps/SpringbootIn10StepsApplication.java
+++ springboot-in-10-steps/src/main/java/com/in28minutes/springboot/basics/springboot_in_10_steps/SpringbootIn10StepsApplication.java
@@ -2,12 +2,17 @@ package com.in28minutes.springboot.basics.springboot_in_10_steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootIn10StepsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+		
+		for(String beanName : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(beanName);
+		}
 	}
 
 }
